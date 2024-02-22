09ca13d4a6c1d3cddfcf163d2636ee3ce51cef26
camligo <camilla.ligovic@gmail.com>
2024-02-22 15:28:33 +1100

    changed scroll

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,11 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+const button = document.querySelector("#click-me")
+button.addEventListener("click", (event) => {
+  console.log(event);
+  event.currentTarget.classList.remove("btn-primary");
+  event.currentTarget.classList.add("btn-danger");
+  event.currentTarget.innerText = "Bingo!";
+})
