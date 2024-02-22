bbfa2e62d5a95f659a96e573aec8a8d77461e158
camligo <camilla.ligovic@gmail.com>
2024-02-22 18:17:13 +1100

    js typescript welcome message

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,10 +8,74 @@ window.Stimulus   = application
 
 export { application }
 
-const button = document.querySelector("#click-me")
-button.addEventListener("click", (event) => {
-  console.log(event);
-  event.currentTarget.classList.remove("btn-primary");
-  event.currentTarget.classList.add("btn-danger");
-  event.currentTarget.innerText = "Bingo!";
-})
+// Nav links scrolling
+
+function scrollToElement(selector, offset) {
+  const element = document.querySelector(selector);
+  if (element) {
+      const offsetTop = element.offsetTop - offset;
+      window.scrollTo({
+          top: offsetTop,
+          behavior: 'smooth'
+      });
+  }
+}
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', event => {
+      event.preventDefault();
+      const target = event.currentTarget.getAttribute('href');
+      scrollToElement(target, 100);
+  });
+});
+
+// Arrow down
+
+document.addEventListener('DOMContentLoaded', function() {
+  const scrollIcon = document.getElementById('arrow-down');
+  const aboutSection = document.getElementById('about-me');
+
+  // Scroll to the next section
+  scrollIcon.addEventListener('click', function() {
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// Typewriter function
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = ["a full stack developer", "from Sweden", "a coder", "Sydney-based"];
+const el = document.getElementById('typewriter');
+
+let sleepTime = 80;
+
+let curPhraseIndex = 0;
+
+const typeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+
+    }
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+typeLoop();
